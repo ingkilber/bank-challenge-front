a0ba46fc2f3fb4744f5c2171ed7606f888fa0704
Kilber Marcano <94709159+ingkilber@users.noreply.github.com>
2022-10-08 19:45:48 -0300

    update navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,19 +7,19 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-sm navbar-dark bg-success justify-content-center">
   <ul className="navbar-nav">
     <li className="nav-item">
-      <a className="nav-link" href="#"><h4>Cuestas</h4></a>
+      <a className="nav-link fs-3" href="#">Cuestas</a>
     </li>
     <li className="nav-item">
-      <a className="nav-link" href="#"><h4>Transferencias</h4></a>
+      <a className="nav-link fs-3" href="#">Transferencias</a>
     </li>
     <li className="nav-item">
-      <a className="nav-link" href="#"><h4>Tarjetas</h4></a>
+      <a className="nav-link fs-3" href="#">Tarjetas</a>
     </li>
     <li className="nav-item">
-      <a className="nav-link" href="#"><h4>Inversiones</h4></a>
+      <a className="nav-link fs-3" href="#">Inversiones</a>
     </li>
     <li className="nav-item">
-      <a className="nav-link" href="#"><h4>Préstamos</h4></a>
+      <a className="nav-link fs-3" href="#">Préstamos</a>
     </li>
   </ul>
 </nav>
